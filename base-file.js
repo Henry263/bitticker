8cb7eabd0fbc6eb0dc5c93cb222f8f1362bae492
Harshil Pate <harshilpatel@prokarma.local>
2017-12-07 22:27:57 -0800

    Update the notification for all coin

--- base-file.js
+++ base-file.js
@@ -4,25 +4,39 @@ $(function() {
 
 
     var btcUrl = "https://api.coindesk.com/v1/bpi/currentprice.json";
+    var allCoins = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD";
+
+    /*
+    $.getJSON(allCoins, function(jsonData) {
+        console.log(jsonData);
+    });
+    */
 
     function myTimer() {
         console.log(' each 1 minute...');
-        getBtcPrice(btcUrl, function(returndata) {
+        getBtcPrice(allCoins, function(returndata) {
             //received data!
-            console.log(JSON.stringify(returndata));
-            bitcoinProce = JSON.stringify(returndata.bpi.USD.rate);
+            //console.log(JSON.stringify(returndata));
+            //bitcoinProce = JSON.stringify(returndata.bpi.USD.rate);
+            var btcPrice = returndata.BTC.USD
+            var ethPrice = returndata.ETH.USD
+            var ltcPrice = returndata.LTC.USD
 
-            $.notify("Bitcoin Ticker", {
-                title: "This is current BTC price!" + bitcoinProce
+
+            var notificationString = "Bitcoin Price :: " + btcPrice + " Etherium Price :: " + ethPrice + " Litecoin Price :: " + ltcPrice;
+            console.log(notificationString);
+        
+            $.notify("Coins update", {
+                title: notificationString
             });
-        })
 
+        });
     }
 
     var myVar = setInterval(myTimer, 60000);
 
-    function getBtcPrice(btcUrl, callback) {
-        $.getJSON(btcUrl, function(returndata) {
+    function getBtcPrice(allCoins, callback) {
+        $.getJSON(allCoins, function(returndata) {
             callback(returndata);
         });
     }
