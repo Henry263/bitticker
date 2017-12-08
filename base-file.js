2008fd08849aa9c74b1bf1359b44b5f6c2bfe3ff
Henry263 <hrpatel.263@gmail.com>
2017-12-07 20:59:51 -0800

    updaate the global var

--- base-file.js
+++ base-file.js
@@ -6,22 +6,13 @@ $(function() {
 
     var geocodingAPI = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
-    $.getJSON(geocodingAPI, function(json) {
-
-        console.log(JSON.stringify(json));
-        var bitcoinProce = JSON.stringify(json.bpi.USD.rate)
-
-    });
-
-
-
     function myTimer() {
         console.log(' each 1 minute...');
-
+         var bitcoinProce;
         $.getJSON(geocodingAPI, function(json) {
 
             console.log(JSON.stringify(json));
-            var bitcoinProce = JSON.stringify(json.bpi.USD.rate)
+            bitcoinProce = JSON.stringify(json.bpi.USD.rate)
 
         });
         $.notify("Custom Title", {
